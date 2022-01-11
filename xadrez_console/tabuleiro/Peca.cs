31aa62573025b34d1a8e3c6e79ae837f0133c1f0
Taiany Brazil <braziltaiany@outlook.com>
2022-01-11 15:50:04 -0300

    Comecando a colocar pecas no tabuleiro

--- xadrez_console/tabuleiro/Peca.cs
+++ xadrez_console/tabuleiro/Peca.cs
@@ -7,12 +7,14 @@
         public int qteMovimentos { get; protected set; }
         public Tabuleiro tab { get; protected set; }
 
-        public Peca(Posicao posicao, Tabuleiro tab, Cor cor)
+        public Peca(Tabuleiro tab, Cor cor)
         {
-            this.posicao = posicao;
+            this.posicao = null;
             this.cor = cor;
             this.tab = tab;
             this.qteMovimentos = 0;
         }
+
+
     }
 }
