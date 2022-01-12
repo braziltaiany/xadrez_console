f0fb6460aff718dafddd365f80de34a5b53382ff
Taiany Brazil <braziltaiany@outlook.com>
2022-01-12 16:13:06 -0300

    Implementando a verificacao de xeque

--- xadrez_console/tabuleiro/Peca.cs
+++ xadrez_console/tabuleiro/Peca.cs
@@ -19,6 +19,10 @@
         {
             qteMovimentos++;
         }
+        public void decrementarQteMovimentos()
+        {
+            qteMovimentos--;
+        }
 
         public bool existeMovimentosPossiveis()
         {
