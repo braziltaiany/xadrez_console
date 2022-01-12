b98e748b506a28c47ad6eb79a532a172f4779de9
Taiany Brazil <braziltaiany@outlook.com>
2022-01-12 18:50:44 -0300

    Jogada especial promocao

--- xadrez_console/xadrez/PartidaDeXadrez.cs
+++ xadrez_console/xadrez/PartidaDeXadrez.cs
@@ -131,10 +131,26 @@ namespace xadrez
         public void realizaJogada(Posicao origem, Posicao destino)
         {
             Peca pecaCapturada = executaMovimenta(origem, destino);
+           
             if (estaEmXeque(jogadorAtual))
             {
                 desfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
+            }
+
+            Peca p = tab.peca(destino);
+
+            // #jogadaespecial promocao
+            if (p is Peao)
+            {
+                if ((p.cor == Cor.Branca && destino.linha == 0) || (p.cor == Cor.Preta && destino.linha == 7))
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
             }
 
 
@@ -158,7 +174,6 @@ namespace xadrez
 
             }
             
-            Peca p = tab.peca(destino);
 
             // #jogadaespecial en passant
             if (p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2))
